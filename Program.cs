a3d9c0f9aaea42935c107eb11cc61aaf26ef4608
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-08 18:10:48 +0100

    Polymorphism ex2

--- Program.cs
+++ Program.cs
@@ -8,31 +8,40 @@ namespace SoloLearn
 {
     class Program
     {
-        class Shape {
-            public virtual void Draw() {
-                Console.Write("Base Draw");
-            }
-        }
-        class Circle : Shape {
-            public override void Draw() {
-                // draw a circle...
-                Console.WriteLine("Circle Draw");
-            }
-        }
-        class Rectangle : Shape {
-            public override void Draw() {
-                // draw a rectangle...
-                Console.WriteLine("Rect Draw");
-            }
-        }
         static void Main(string[] args)
         {
-            Shape c = new Circle();
-            c.Draw();
+            Unit unit1 = new Unit();
+            Unit musketeer = new Musketeer();
+            Unit magician = new Magician();
 
-            Shape r = new Rectangle();
-            r.Draw();
+            unit1.Attack();
+            musketeer.Attack();
+            magician.Attack();
+        }
+    }
 
+    class Unit
+    {
+        public virtual void Attack()
+        {
+            Console.WriteLine("Using sword!");
+        }
+    }
+    
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Musketeer:Unit
+    {
+        public override void Attack(){
+            Console.WriteLine("Using musket!");
+        }
+    }
+    /*derive the class from Unit class
+    and override Attack() method*/
+    class Magician : Unit
+    {
+        public override void Attack(){
+            Console.WriteLine("Using magic!");
         }
     }
 }
