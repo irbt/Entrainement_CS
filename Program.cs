6b8c862cceb5a4662e830f897e52480e7b2afa50
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-15 12:00:06 +0100

    Generic class (énoncé)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace SoloLearn
 {
@@ -10,26 +11,23 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            try
-            {
-                int drinks = Convert.ToInt32(Console.ReadLine());
-                int shelves = Convert.ToInt32(Console.ReadLine());
+            string text = "PYthon";
+            int intNum = 3;
+            double doubNum = Convert.ToDouble(Console.ReadLine());
 
-                //your code goes here
 
-            }
-            /*
-             * 1. DivideByZeroException => "At least 1 shelf"
-             * 2. FormatException => "Please insert an integer"
-            */
-            catch ()
-            {
-
-            }
-            catch ()
-            {
-
-            }
+            Printer.Print(text);
+            Printer.Print(intNum);
+            Printer.Print(doubNum);
         }
     }
+    class Printer
+    {
+        //your code goes here
+       
+        }
+        
+        
+    
+
 }
