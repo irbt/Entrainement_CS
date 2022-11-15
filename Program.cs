f5a1c95cc37adcf1bf116d39d0c5bdbe9bc094d0
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-15 18:46:56 +0100

    List<T>
    avant test fonctions additionnelles

--- Program.cs
+++ Program.cs
@@ -3,41 +3,25 @@ using System.Collections.Generic;
 
 namespace SoloLearn
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            Elems<string> elems1 = new Elems<string>();
-            elems1.Add("John", "Tamara", "David");
-            elems1.Show();
-
-            Console.WriteLine();
-
-            Elems<int> elems2 = new Elems<int>();
-            elems2.Add(5, 14, 13);
-            elems2.Show();
-
-        }
-    }
-    //make this class generic
-    class Elems <T>
-    {
-        public T[] elements = new T[3];
-
-        public void Add (T elem1, T elem2, T elem3)
-        {
-            elements[0] = elem1;
-            elements[1] = elem2;
-            elements[2] = elem3;
-        }
-
-        public void Show()
-        {
-            foreach (T item in elements)
-            {
-                Console.Write(item + " ");
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		    List<int> li = new List<int>();
+            li.Add(59);
+            li.Add(72);
+            li.Add(95);
+            li.Add(5);
+            li.Add(9);
+            li.RemoveAt(1); // remove 72
+            
+            Console.Write("\nList: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); // 59  95  5  9
+            li.Sort();
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); // 5  9  59  95
+		}
+	}
 }
