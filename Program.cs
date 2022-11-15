e9725d1a8b044cd1063971de3022bd92dcd3f3a1
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-15 16:56:27 +0100

    Generic class
    Enoncé TP

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 namespace SoloLearn
 {
@@ -11,26 +7,37 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            string text = "Python tuile";
-            int intNum = 3;
-            double doubNum = 3.8;
 
+            Elems<string> elems1 = new Elems<string>();
+            elems1.Add("John", "Tamara", "David");
+            elems1.Show();
 
-            Printer.Print<string>(ref text);
-            Printer.Print<int>(ref intNum);
-            Printer.Print<double>(ref doubNum);
-            Printer.Print<double , int>(ref doubNum , ref intNum);
+            Console.WriteLine();
+
+            Elems<int> elems2 = new Elems<int>();
+            elems2.Add(5, 14, 13);
+            elems2.Show();
+
         }
     }
-    class Printer
+    //make this class generic
+    class Elems
     {
-        //your code goes here
-        public static void Print<T>( ref T a){
-            Console.WriteLine("Showing " + a);
+        public int[] elements = new int[3];
+
+        public void Add(int elem1, int elem2, int elem3)
+        {
+            elements[0] = elem1;
+            elements[1] = elem2;
+            elements[2] = elem3;
         }
-        
-        public static void Print<T , U>( ref T a , ref U b){
-            Console.WriteLine("Showing " + a + " + " + b);
+
+        public void Show()
+        {
+            foreach (int item in elements)
+            {
+                Console.Write(item + " ");
+            }
+        }
     }
-}
 }
