efe56c2fc7fec848046d828a7a04a9ff5aa87480
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-16 14:16:21 +0100

    List <T> + Méthodes additionnelles

--- Program.cs
+++ Program.cs
@@ -19,9 +19,50 @@ namespace SoloLearn
             for (int x = 0; x < li.Count; x++)
                 Console.Write(li[x] + " "); // 59  95  5  9
             li.Sort();
+            //li.Clear();
             Console.Write("\nSorted: ");
             for (int x = 0; x < li.Count; x++)
                 Console.Write(li[x] + " "); // 5  9  59  95
+            
+            int test = 77;
+
+            //-------------------------------------------------------------
+            li.Insert(2, test);
+            li.Add(9);
+
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); // 5  9  59  95
+            // -------------------------------------------------
+            li.Remove(9);
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); 
+
+            // -------------------------------------------------
+            li.TrimExcess();
+            //-------------------------------
+            /*Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Entrez le numéro à chercher");
+            int s =  Convert.ToInt32( Console.ReadLine());
+            if (li.Contains(s)){
+                Console.WriteLine("true");
+            }else {
+                Console.WriteLine("pas trouvé");
+
+            };
+
+            Console.WriteLine("----------------------------------------------");*/
+            li.Insert(5, 977 );
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); 
+
+            li.Insert(6, 66 ); // plante si index>6
+            Console.Write("\nSorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.Write(li[x] + " "); 
+
 		}
 	}
 }
