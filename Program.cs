decd9bb27b0889b6330a607ccac949db15b5cf6f
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-16 15:03:45 +0100

    Top Of The Leaderboard (corrigé)

--- Program.cs
+++ Program.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 
 namespace SoloLearn
 {
-    /*You need to write a program for the game to sort player scores.
-The program you are given takes N number as input, which represents the number of players, and defines a score list.
-Complete the program to take N count of numbers (the scores) as input, store them in a scores list, sort and output them, each separated by a space.*/
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entrez nb de score");
             int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine ("nb players = " + numOfPlayers);
 
             List<int> scores = new List<int>();
             int count = 0;
-            while (count<numOfPlayers)
+            while (count < numOfPlayers)
             {
+                Console.WriteLine("Entrez un score");
+
                 int score = Convert.ToInt32(Console.ReadLine());
                 //your code goes here
+                //Console.WriteLine("Boucle avant incr " + count);
+                scores.Add(score);
+               //Console.WriteLine(scores[count]);
+                count++;
+                //Console.WriteLine("Boucle après incr " + count);
                 
             }
 
             //sort the list and output elements
+            scores.Sort();
+            
+            
+            for (int i=0 ; i < count ; i++){
+                Console.Write ( scores [i] + " ");
+                
+            }
             
         }
     }
