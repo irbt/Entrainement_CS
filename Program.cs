ec39222d576d7731dea47395dc322252de90ffe1
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-17 15:18:04 +0100

    Queue (corrigé)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,22 +14,29 @@ namespace SoloLearn
         {
             Queue<int> q = new Queue<int>();
 
+                Console.WriteLine("Entrez 3 nombres");
             while (q.Count<3)
             {
+                Console.WriteLine("Chiffre n°" + (q.Count+1));
                 int num = Convert.ToInt32(Console.ReadLine());
                 //your code goes here
+                q.Enqueue(num);
                 
             }
 
             Console.Write("Queue: ");
-            foreach (int i in q)
-                Console.Write(i + " ");
+            foreach (int i in q){
+                Console.Write(i + " ");}
 
             Console.WriteLine();
             
             Console.Write("Sorted: ");
             //your code goes here
+            int [] arr = q.ToArray();
+            Array.Sort(arr);
             
+            foreach (int j in arr){
+                Console.Write(j + " ");}
         }
     }
 }
