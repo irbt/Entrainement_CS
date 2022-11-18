4277c2a6b83a194b181cec21e5ea5296d528f9cf
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-18 11:42:25 +0100

    Cas pratique fin de formation
    A coffee shop manager is running a promotion and wants to offer a discount for coffee drinks.
    The program you are given takes the discount value as input and defines a dictionary, where the names of the coffee drinks are set as keys, and their prices are set as values.
    Write a program to discount all of the prices and output a new price list in the format shown below.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace SoloLearn
 {
@@ -12,31 +10,21 @@ namespace SoloLearn
     {
         static void Main(string[] args)
         {
-            Queue<int> q = new Queue<int>();
+            int discount = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Entrez 3 nombres");
-            while (q.Count<3)
-            {
-                Console.WriteLine("Chiffre n°" + (q.Count+1));
-                int num = Convert.ToInt32(Console.ReadLine());
-                //your code goes here
-                q.Enqueue(num);
-                
-            }
+            Dictionary<string, int> coffee = new Dictionary<string, int>();
+            coffee.Add("Americano", 50);
+            coffee.Add("Latte", 70);
+            coffee.Add("Flat White", 60);
+            coffee.Add("Espresso", 60);
+            coffee.Add("Cappuccino", 80);
+            coffee.Add("Mocha", 90);
 
-            Console.Write("Queue: ");
-            foreach (int i in q){
-                Console.Write(i + " ");}
 
-            Console.WriteLine();
-            
-            Console.Write("Sorted: ");
             //your code goes here
-            int [] arr = q.ToArray();
-            Array.Sort(arr);
             
-            foreach (int j in arr){
-                Console.Write(j + " ");}
+
+
         }
     }
 }
